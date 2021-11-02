84fa3fb89900113ce7dff4146c152c616c29b5b1
Angel Jimenez <include.bruce@gmail.com>
2021-11-02 22:25:48 +0800

    Add findAdopter method in the Adopter local interface

--- hsport-catalog-ejb/src/main/java/apc/entjava/hssportscatalogejb/AdopterLocal.java
+++ hsport-catalog-ejb/src/main/java/apc/entjava/hssportscatalogejb/AdopterLocal.java
@@ -8,6 +8,7 @@ public interface AdopterLocal {
     public List<AdopterEntity> getAdopter();
     public void addAdopter(AdopterEntity adopter);
     public void removeAdopter(AdopterEntity adopter);
+    public AdopterEntity findAdopter(Long adopterId);
     // public void updateAdopter(AdopterEntity adopter);
     public List<AdopterEntity> searchByFirstName(String firstName);
     public List<AdopterEntity> searchByLastName(String lastName);
