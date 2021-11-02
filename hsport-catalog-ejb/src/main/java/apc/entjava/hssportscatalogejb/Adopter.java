e1bf43daa41062079dab5c5489290e0e96cbf4de
Angel Jimenez <include.bruce@gmail.com>
2021-11-02 22:03:51 +0800

    Add EJB implementation adopter with stub methods

---
+++ hsport-catalog-ejb/src/main/java/apc/entjava/hssportscatalogejb/Adopter.java
@@ -0,0 +1,31 @@
+package apc.entjava.hssportscatalogejb;
+
+import java.util.List;
+
+public class Adopter implements AdopterLocal {
+
+    @Override
+    public List<AdopterEntity> getAdopter() {
+        return null;
+    }
+
+    @Override
+    public void addAdopter(AdopterEntity adopter) {
+
+    }
+
+    @Override
+    public void removeAdopter(AdopterEntity adopter) {
+
+    }
+
+    @Override
+    public List<AdopterEntity> searchByFirstName(String firstName) {
+        return null;
+    }
+
+    @Override
+    public List<AdopterEntity> searchByLastName(String lastName) {
+        return null;
+    }
+}
