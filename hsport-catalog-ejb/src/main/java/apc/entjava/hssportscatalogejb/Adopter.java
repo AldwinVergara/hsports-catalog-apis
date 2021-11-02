9bc5791158d0bbd7bc01cd8125c2d0dc8cc17351
Angel Jimenez <include.bruce@gmail.com>
2021-11-02 22:26:41 +0800

    Implement methods dictated in the interface

--- hsport-catalog-ejb/src/main/java/apc/entjava/hssportscatalogejb/Adopter.java
+++ hsport-catalog-ejb/src/main/java/apc/entjava/hssportscatalogejb/Adopter.java
@@ -4,6 +4,7 @@ import javax.ejb.LocalBean;
 import javax.ejb.Singleton;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Singleton
@@ -19,26 +20,39 @@ public class Adopter implements AdopterLocal {
 
     @Override
     public List<AdopterEntity> getAdopter() {
-        return null;
+        return this.entityManager.createQuery("select p from AdopterEntity p", AdopterEntity.class).getResultList();
     }
 
     @Override
     public void addAdopter(AdopterEntity adopter) {
-
+        this.entityManager.persist(adopter);
     }
 
     @Override
     public void removeAdopter(AdopterEntity adopter) {
+        this.entityManager.remove(this.entityManager.contains(adopter) ? adopter : this.entityManager.merge(adopter));
+    }
 
+    @Override
+    public AdopterEntity findAdopter(Long adopterId) {
+        return this.entityManager.find(AdopterEntity.class, adopterId);
     }
 
     @Override
     public List<AdopterEntity> searchByFirstName(String firstName) {
-        return null;
+        TypedQuery<AdopterEntity> query = this.entityManager.createQuery("select p from AdopterEntity p" +
+                " where p.firstName like :firstName", AdopterEntity.class);
+
+        return query.setParameter("firstName", "%" + firstName + "%").getResultList();
     }
 
     @Override
     public List<AdopterEntity> searchByLastName(String lastName) {
-        return null;
+        TypedQuery<AdopterEntity> query = this.entityManager.createQuery("select p from AdopterEntity p" +
+                " where p.firstName like :lastName", AdopterEntity.class);
+
+        return query.setParameter("lastName", "%" + lastName + "%").getResultList();
     }
+
+
 }
