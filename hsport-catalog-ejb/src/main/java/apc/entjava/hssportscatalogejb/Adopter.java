0312f321a395693886887e3efde823f4af51b6e7
Angel Jimenez <include.bruce@gmail.com>
2021-11-02 22:08:53 +0800

    Add annotations and empty constructor. Inject EntityManager

--- hsport-catalog-ejb/src/main/java/apc/entjava/hssportscatalogejb/Adopter.java
+++ hsport-catalog-ejb/src/main/java/apc/entjava/hssportscatalogejb/Adopter.java
@@ -1,8 +1,21 @@
 package apc.entjava.hssportscatalogejb;
 
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
+@Singleton
+@LocalBean
 public class Adopter implements AdopterLocal {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Adopter() {
+
+    }
 
     @Override
     public List<AdopterEntity> getAdopter() {
